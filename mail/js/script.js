27810545d541141727d0a2d4d5a9b3a2b9e218e6
Ciuff4 <gasperini_cristina@libero.it>
2022-04-19 15:27:41 +0200

    sistemato lavoro di logica in inserimento mail e stampa in html

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,18 +7,26 @@
 
 let email= prompt ("Inserisci la tua mail");
 
-const arrAuthorized =["zrxr@nalsci.com","timoninstas@ichstet.com","angiloxlkaz@livegolftv.com", "cjs016@bookug.site"];
+const arrAuthorized =["mail1","mail2","mail3"];
 
 let verifiedMail=false;
 
 for(let i=0; i<arrAuthorized.length; i++){
 
-    console.log(i);
-
     if(email===arrAuthorized[i]){
-        verifiedMail=true;
-        document.getElementById("output").innerHTML= "Congratulazioni, la mail inserita è corretta";
-    }else{
-        document.getElementById("output").innerHTML= "Spiacenti, la mail inserita non è corretta, la preghiamo di riprovare";
-        }
+        verifiedMail=true
+        console.log("mail trovata");
+    }
 }
+
+let message;
+
+if((verifiedMail===true)){
+    console.log("found");
+    message=`Congratulazioni, la mail inserita è corretta`
+}else{
+    console.log("not found");
+    message=`Spiacenti, la mail inserita non è corretta, la preghiamo di riprovare`
+}
+
+document.getElementById("output").innerHTML=message
